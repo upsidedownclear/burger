aa26a14100019d0535cd25955d46992360d8bcc9
upsidedownclear <karrotgoku6@gmail.com>
2019-07-15 14:15:43 +0200

    Updates

---
+++ precache-manifest.3fe5f623586e8994a6111e0d20c6e430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df33486efea50d4707238c5b3d9c3706",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "02dd8de901471f1d578a",
+    "url": "/burger/static/css/main.7ffa2bda.chunk.css"
+  },
+  {
+    "revision": "b26129f75834355090cc",
+    "url": "/burger/static/js/2.5e3b1a22.chunk.js"
+  },
+  {
+    "revision": "02dd8de901471f1d578a",
+    "url": "/burger/static/js/main.fc498072.chunk.js"
+  },
+  {
+    "revision": "5ae864a761ffe3b0b200",
+    "url": "/burger/static/js/runtime~main.a953f988.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
